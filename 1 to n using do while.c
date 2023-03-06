203c8d6131c5135da12427488e0e96cc4ad57485
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-06 14:00:41 +0530

    Add files via upload

---
+++ 1 to n using do while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=1, end;
+
+    printf("enter numbers from 1 to : ");
+    scanf("%d", &end);
+
+    
+    while(i<=end)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+
+}      
+    
+
